86681328a7f9cdeb348ffa857b9ea72d273143c1
Jeshua Arias <jessariasmtz@gmail.com>
2020-01-13 11:43:26 -0500

    var dump

--- app/models/clientes/getEmpresa.php
+++ app/models/clientes/getEmpresa.php
@@ -8,7 +8,7 @@ try {
     $result = $conn->prepare("SELECT * FROM Empresas;");
     $result->execute();
     $conn = null;
-
+    var_dump($result);
     exit(json_encode($result->fetchAll(PDO::FETCH_ASSOC)));
 } catch(Exception $e) {
     echo "getAllCitas: $e->getMessage()";
